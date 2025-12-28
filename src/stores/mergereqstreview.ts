4db94013c9dd74765086246004d54f92e7fb8271
Amiya167 <neuvillette-fontaine@outlook.com>
2025-12-28 17:24:56 +0800

    fix typos

--- src/stores/mergereqstreview.ts
+++ src/stores/mergereqstreview.ts
@@ -1,7 +1,7 @@
 /*
 * Merge Requests Review Store
 * TODO:此处补充 Merge Requests Review Store 的状态定义
-*  支持Github和Gitlab,和正在开发的Nasti CodeStore,包括但不限于:
+*  支持Github和Gitlab,和正在开发的Ptilopsis CodeStore,包括但不限于:
 * - 合并请求列表
 *  - 选中的合并请求ID
 * - 合并请求详情
